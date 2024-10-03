33bf9319f0bea435aa4965a721267e27f8fad6ec
Michael Braun <michael@web4brauns.de>
2024-10-03 22:55:58 +0200

    Add html sanitization

--- src/app/(embedded)/ccm/[serviceId]/threads/[threadId]/messages/[messageId]/embed/_components/html.component.tsx
+++ src/app/(embedded)/ccm/[serviceId]/threads/[threadId]/messages/[messageId]/embed/_components/html.component.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import styles from './html.module.scss';
 
+import { JSDOM } from 'jsdom';
+import DOMPurify from 'dompurify';
+import * as sanitizeHtml from 'sanitize-html';
+
 type HtmlProps = {
   content: string;
 };
@@ -8,10 +12,15 @@ type HtmlProps = {
 const Html: React.FunctionComponent<HtmlProps> = ({
   content,
 }) => {
+  const window = new JSDOM('').window;
+  const purify = DOMPurify(window);
+  const cleanContent = purify.sanitize(content);
+  const cleanCleanContent = sanitizeHtml.default(cleanContent);
+
   return (
     <div
       className={styles.root}
-      dangerouslySetInnerHTML={{ __html: content }}
+      dangerouslySetInnerHTML={{ __html: cleanCleanContent }}
     />
   );
 };
