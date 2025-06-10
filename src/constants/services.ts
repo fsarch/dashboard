1e7aacf5f2b5dd1ba0ebfe6dcb1d8f984a04f102
Michael Braun <michael@web4brauns.de>
2025-06-10 23:30:06 +0200

    Fix site navigation of product

--- src/constants/services.ts
+++ src/constants/services.ts
@@ -32,44 +32,52 @@ export const SERVICES: Record<EServiceType, {
     name: 'Product',
     basePath: '/product',
     navigation: [{
-      name: 'Home',
+      name: 'Übersicht',
       path: '/',
+      icon: 'layer-group',
     }, {
       name: 'Kataloge',
       path: '/catalog',
+      icon: 'book',
     }, {
       name: 'Lokalisierungen',
       path: '/localization',
+      icon: 'language',
     }],
     routes: {
       '/catalog/:catalogId{/*path}': {
         navigation: [{
-          name: 'Up',
+          name: 'Katalog-Übersicht',
           path: '/',
+          icon: 'book',
         }, {
           name: 'Übersicht',
           path: {
             $type: 'jsonata',
             value: "'/catalog/' & params.catalogId",
           },
+          icon: 'layer-group',
         }, {
           name: 'Items',
           path: {
             $type: 'jsonata',
             value: "'/catalog/' & params.catalogId & '/item'",
           },
+          icon: 'sitemap',
         }, {
           name: 'Attributes',
           path: {
             $type: 'jsonata',
             value: "'/catalog/' & params.catalogId & '/attribute'",
           },
+          icon: 'hashtag',
         }, {
           name: 'Element-Types',
           path: {
             $type: 'jsonata',
             value: "'/catalog/' & params.catalogId & '/item-type'",
           },
+          icon: 'puzzle-piece',
         }],
       },
     },
