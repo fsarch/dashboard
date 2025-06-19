1d2380839c70dd502c92fb6912e6386ad12b79a5
Michael Braun <michael@web4brauns.de>
2025-06-19 21:37:54 +0200

    Improve pdf-server api

--- src/app/(with-header)/function/[serviceId]/function/[functionId]/_components/editor-types/services/pdf-server-api.d.ts
+++ src/app/(with-header)/function/[serviceId]/function/[functionId]/_components/editor-types/services/pdf-server-api.d.ts
@@ -2,7 +2,39 @@
 // interface: PdfServerApi
 
 namespace FsArchApiCatalog {
+  type RenderPdfOptions = {
+    /**
+     * Sets the viewport of the document on which the PDF should be generated.
+     * The viewport could change the positioning and rendering of the given html (e.g. mobile view).
+     */
+    viewport: {
+      /**
+       * The width of the viewport in px
+       */
+      width: number;
+      /**
+       * The height of the viewport in px
+       */
+      height: number;
+    };
+    /**
+     * Specifies the format and settings of the generated PDF file
+     */
+    export: {
+      /**
+       * Use a fixed format (e.g. DIN A4, DIN A5)
+       */
+      format: 'A4';
+    };
+  };
+
   declare interface PdfServerApi {
-    renderPdf(): Promise<void>;
+
+    /**
+     * Creates a pdf document from the given html code
+     * @param html
+     * @param options
+     */
+    renderPdf(html: string, options?: RenderPdfOptions): Promise<void>;
   }
 }
