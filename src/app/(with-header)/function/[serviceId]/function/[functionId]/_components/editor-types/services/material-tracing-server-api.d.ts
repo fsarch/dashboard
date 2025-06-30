8e50f62ac1ca276527c73c3712bd4da618e68b30
Michael Braun <michael@web4brauns.de>
2025-07-01 00:12:45 +0200

    Add new api

--- src/app/(with-header)/function/[serviceId]/function/[functionId]/_components/editor-types/services/material-tracing-server-api.d.ts
+++ src/app/(with-header)/function/[serviceId]/function/[functionId]/_components/editor-types/services/material-tracing-server-api.d.ts
@@ -6,6 +6,13 @@ namespace FsArchApiCatalog {
     id: string;
     name: string;
     externalId?: string;
+    partTypeId: string;
+  };
+
+  type PartType = {
+    id: string;
+    name: string;
+    externalId?: string;
   };
 
   interface MaterialTracingPartsApi {
@@ -16,10 +23,25 @@ namespace FsArchApiCatalog {
     get(partId: string): Part;
   }
 
+  interface MaterialTracingPartTypesApi {
+    /**
+     * Receive a single part-type by its ID
+     * @param partId
+     */
+    get(partId: string): PartType;
+  }
+
   declare interface MaterialTracingServerApi {
     /**
      * Manage parts inside the Material-Tracing server
      */
     readonly parts: MaterialTracingPartsApi;
+  }
+
+  declare interface MaterialTracingServerApi {
+    /**
+     * Manage part-types inside the Material-Tracing server
+     */
+    readonly partTypes: MaterialTracingPartTypesApi;
   }
 }
