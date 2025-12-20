07343033375d228d0a1063f151c5d1c5b297432a
Michael Braun <michael@web4brauns.de>
2025-12-21 00:23:24 +0100

    Add force-dynamic

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from 'next'
 import { getThemeConfiguration } from "@/utils/configuration.utils";
 
+export const dynamic = 'force-dynamic';
+
 export default async function manifest(): Promise<MetadataRoute.Manifest> {
   const theme = await getThemeConfiguration();
 
