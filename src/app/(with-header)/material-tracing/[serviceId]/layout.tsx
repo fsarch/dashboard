4f870fbd43e75807e6758bf2c783104dce88217c
Michael Braun <michael@web4brauns.de>
2025-12-21 01:20:59 +0100

    Fix floating button style

--- src/app/(with-header)/material-tracing/[serviceId]/layout.tsx
+++ src/app/(with-header)/material-tracing/[serviceId]/layout.tsx
@@ -1,6 +1,7 @@
 import type { PropsWithChildren } from "react";
 import ShortCodeScanFloatingButton
   from "@/app/(with-header)/material-tracing/[serviceId]/_components/floating-button/ShortCodeScanFloatingButton";
+import styles from './layout.module.scss';
 
 export default async function RootLayout(props: PropsWithChildren) {
   const {
@@ -8,9 +9,9 @@ export default async function RootLayout(props: PropsWithChildren) {
   } = props;
 
   return (
-    <>
+    <div className={styles.root}>
       {children}
       <ShortCodeScanFloatingButton />
-    </>
+    </div>
   );
 }
